6380033bbf356ddd5f4396bd03be30aa75be10ba
Carlos Amaral <carlos.giovaniamaral@outlook.comgit config --global user.email carlos.giovaniamaral@outlook.com>
2024-06-28 21:39:03 -0300

    feat: create and delete users

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,34 +1,64 @@
-
 import './styles.css'
 import Trash from '../../assets/lixeira.svg'
 import api from '../../services/api'
 import { useEffect, useState } from 'react'
 
-
 function Home() {
-
   const [users, setUsers] = useState([])
+  const [name, setName] = useState('')
+  const [age, setAge] = useState('')
+  const [email, setEmail] = useState('')
 
-  async function getUsers(){
-    const usersFromApi = await api.get('/usuarios')
-    setUsers(usersFromApi.data)
-  
+  async function getUsers() {
+    const response = await api.get('/usuarios')
+    setUsers(response.data)
   }
-useEffect(()=> {
-  getUsers()
-},[])
-  
 
+  async function createUser() {
+    await api.post('/usuarios', { name, age, email })
+    setName('')
+    setAge('')
+    setEmail('')
+    getUsers()
+  }
+
+  async function deleteUser(id) {
+    await api.delete(`/usuarios/${id}`)
+    getUsers()
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
 
   return (
     <div className='container'>
-      <form>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        createUser()
+      }}>
         <h1>Cadastro de Usuários</h1>
-        <input placeholder='Nome' name='name' type='text' />
-        <input placeholder='Idade' name='idade' type='number' />
-        <input placeholder='Email' name='email' type='email' />
-        <button type='button'>Cadastrar</button>
+        <input
+          placeholder='Nome'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type='text'
+        />
+        <input
+          placeholder='Idade'
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+          type='text'
+        />
+        <input
+          placeholder='Email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type='email'
+        />
+        <button type='submit'>Cadastrar</button>
       </form>
+
       {users.map((user) => (
         <div key={user.id} className='card'>
           <div>
@@ -42,13 +72,11 @@ useEffect(()=> {
               Email: <span>{user.email}</span>
             </p>
           </div>
-          <button>
-            <img src={Trash} />
+          <button onClick={() => deleteUser(user.id)}>
+            <img src={Trash} alt="Delete" />
           </button>
         </div>
       ))}
-
-
     </div>
   )
 }
